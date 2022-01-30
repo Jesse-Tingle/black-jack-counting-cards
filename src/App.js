36ac4c6c4ae06acde2f0faca90d793f7e7491143
Jesse-Tingle <jtingle0@gmail.com>
2022-01-30 01:10:26 -0500

    sets up boilerplate, basic background for game, buttons, and basic structure for score keeping

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import "./App.css"
+import suiteIcons from "./components/img/bg-hsdc.png"
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<div className="title">BLACKJACK</div>
+				<div className="score">
+					<div className="wins">
+						<div className="w">Wins</div>
+						<div className="w-score">0</div>
+					</div>
+					<div className="losses">
+						<div className="l">Losses</div>
+						<div className="l-score">0</div>
+					</div>
+					<div className="draws">
+						<div className="d">Draws</div>
+						<div className="d-score">0</div>
+					</div>
+				</div>
+				<div className="card-totals">
+					<div className="user-info">
+						<div className="user">You:</div>
+						<div className="user-card-total">0</div>
+					</div>
+					<div className="dealer-info">
+						<div className="dealer">Dealer:</div>
+						<div className="dealer-card-total">0</div>
+					</div>
+				</div>
+
+				<img className="Suite-Icons" src={suiteIcons} alt="suite icons" />
+
+				<div className="game-buttons">
+					<button className="hit">Hit</button>
+					<button className="stand">Stand</button>
+					<button className="double-down">Double Down</button>
+					<button className="split">Split</button>
+					<button className="surrender">Surrender</button>
+				</div>
+			</header>
+		</div>
+	)
+}
+
+export default App
