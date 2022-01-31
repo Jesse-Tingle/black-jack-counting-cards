892adbb76f1ed66827a7f62fcb725f3eae77c52f
Jesse-Tingle <jtingle0@gmail.com>
2022-01-30 21:45:03 -0500

    adds buttons for placing bets and styling

--- src/App.js
+++ src/App.js
@@ -23,20 +23,40 @@ function App() {
 		<div className="App">
 			<header className="App-header">
 				<div className="title">BLACKJACK</div>
-				<div className="score">
-					<div className="wins">
-						<div className="w">Wins</div>
-						<div className="w-score">0</div>
+				<div className="top">
+					<div className="chips">
+						<div className="row one">
+							<button className="top-chip five">$5</button>
+							<button className="ten">$10</button>
+						</div>
+						<div className="row two">
+							<button className="top-chip twenty">$20</button>
+							<button className="top-chip fifty">$50</button>
+						</div>
+						<div className="row three">
+							<button className="top-chip hundred">$100</button>
+							<button className="five-hundred">$500</button>
+						</div>
 					</div>
-					<div className="losses">
-						<div className="l">Losses</div>
-						<div className="l-score">0</div>
+
+					<div className="score">
+						<div className="wins">
+							<div className="w">Wins</div>
+							<div className="w-score">0</div>
+						</div>
+						<div className="losses">
+							<div className="l">Losses</div>
+							<div className="l-score">0</div>
+						</div>
+						<div className="draws">
+							<div className="d">Draws</div>
+							<div className="d-score">0</div>
+						</div>
 					</div>
-					<div className="draws">
-						<div className="d">Draws</div>
-						<div className="d-score">0</div>
-					</div>
+
+					<button className="end-game">End Game</button>
 				</div>
+
 				<div className="card-totals">
 					<div className="user-info">
 						<div className="user">You:</div>
@@ -51,6 +71,7 @@ function App() {
 				<img className="Suite-Icons" src={suiteIcons} alt="suite icons" />
 
 				<div className="game-buttons">
+					<button className="deal">Deal</button>
 					<button className="hit">Hit</button>
 					<button className="stand">Stand</button>
 					<button className="double-down">Double Down</button>
